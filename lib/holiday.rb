278ca9c6ccd9b7545671d9fa924d0d9214d19951
Keri Deemer <kerid18@att.net>
2019-09-16 05:55:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}"
     holiday.each do |holiday, supplies|
-      
+      puts " #"
 end
 
 def all_holidays_with_bbq(holiday_hash)
